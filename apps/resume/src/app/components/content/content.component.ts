887b1398e25cbe4d7a420ca06ad57c56cc0f34c1
DESKTOP-5GK56BC\flouk <flouk.cs07@gmail.com>
2021-03-29 20:21:50 +0700

    update

--- apps/resume/src/app/components/content/content.component.ts
+++ apps/resume/src/app/components/content/content.component.ts
@@ -3,14 +3,18 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'resume-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.scss']
+  styleUrls: ['./content.component.scss'],
 })
 export class ContentComponent implements OnInit {
-
-  @Input() visibleMenu!:boolean;
-  constructor() { }
+  @Input() visibleMenu!: boolean;
+  constructor() {}
 
   ngOnInit(): void {
+    this.appHeight();
   }
 
+  appHeight() {
+    const doc = document.documentElement;
+    doc.style.setProperty('--content-height', `${window.innerHeight}px`);
+  }
 }
